a45da754754f5bed3488dbd3268a8e36d1722d19
NavedHussain <navedhussain1803@gmail.com>
2023-08-29 23:02:16 +0530

    added songs thprugh js

--- Spotify.js
+++ Spotify.js
@@ -1,20 +1,33 @@
-// console.log("Welcome to Spotify")
-// let song = [
-//     {songName: "Salame-e-Ishq", filePath: "songs/Arijit Singh - Zaalima.mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/Atif Aslam - Pehli Nazar Mein.mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/Atif Aslam - Tu Jaane Na.mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/IAMPRATHEEK - badtameez dil.mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/Jeet Gannguli - Hamari Adhuri Kahani (Title Track) [From -Hamari Adhuri Kahani-].mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/Nusrat Fateh Ali Khan - Kali Kali Zulfon Ke Phande Nah Dalo.mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/Pritam - Channa Mereya.mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/Ricky Mishra - Tu Jaane Ya Na Jaane.mp3", coverPath: "cover/1.jpg"},
-//     {songName: "Salame-e-Ishq", filePath: "songs/Talha Anjum - Two Tone.mp3", coverPath: "cover/1.jpg"}
-// ]
+import songsData from "./songsList.json" assert { type: 'json' }
+const songContainer = document.querySelector(".container ul")
+console.log(songsData)
 
-// let audioElement = new Audio(song[0]);
-// audioElement.play()
+var audio = new Audio(songsData[0].path)
 
-// console.log(song)
 
-import songList from "./songs"
-console.log(songList)
+for (let i = 0 ; i<=songsData.length-1 ; i++){
+    console.log(i);
+    const songItem = document.createElement("li");
+    songContainer.appendChild(songItem);
+    const songDiv1 = document.createElement("div");
+    songItem.appendChild(songDiv1);
+    const songImg = document.createElement("span")
+    const listImg = songImg.classList;
+    listImg.add("songimg")
+    songDiv1.appendChild(songImg)
+    const songName = document.createElement("span")
+    songName.innerHTML = `${songsData[i].name.slice(0, 30)}`;
+    songDiv1.appendChild(songName)
+    
+    const songDiv2 = document.createElement("div");
+    songItem.appendChild(songDiv2);
+    const songTime = document.createElement("span");
+    songTime.innerHTML = "00:00"
+    songDiv2.appendChild(songTime)
+    const songIcon = document.createElement("i");
+    const list = songIcon.classList;
+    list.add("fa-solid");
+    list.add("fa-play");
+    songDiv2.appendChild(songIcon)
+
+}
