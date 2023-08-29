2132d77b05cfa19c922d98fb80a02096274b8bbe
NavedHussain <navedhussain1803@gmail.com>
2023-08-29 21:40:19 +0530

    mryrkyuk

---
+++ Spotify.js
@@ -0,0 +1,20 @@
+// console.log("Welcome to Spotify")
+// let song = [
+//     {songName: "Salame-e-Ishq", filePath: "songs/Arijit Singh - Zaalima.mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/Atif Aslam - Pehli Nazar Mein.mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/Atif Aslam - Tu Jaane Na.mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/IAMPRATHEEK - badtameez dil.mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/Jeet Gannguli - Hamari Adhuri Kahani (Title Track) [From -Hamari Adhuri Kahani-].mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/Nusrat Fateh Ali Khan - Kali Kali Zulfon Ke Phande Nah Dalo.mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/Pritam - Channa Mereya.mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/Ricky Mishra - Tu Jaane Ya Na Jaane.mp3", coverPath: "cover/1.jpg"},
+//     {songName: "Salame-e-Ishq", filePath: "songs/Talha Anjum - Two Tone.mp3", coverPath: "cover/1.jpg"}
+// ]
+
+// let audioElement = new Audio(song[0]);
+// audioElement.play()
+
+// console.log(song)
+
+import songList from "./songs"
+console.log(songList)
